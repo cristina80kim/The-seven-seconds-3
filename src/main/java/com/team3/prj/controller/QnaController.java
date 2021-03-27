b25f58da413638d852867700f7cf41ba0813a1c7
maruhe <maruhe@DESKTOP-9HVMQ11>
2021-03-27 17:54:53 +0900

    // qnacontroller - 기능설명 및 param 입력.

--- src/main/java/com/team3/prj/controller/QnaController.java
+++ src/main/java/com/team3/prj/controller/QnaController.java
@@ -33,22 +33,29 @@ public class QnaController {
 		return service.selectAll();
 	}
 
-	// 기능 : Qna_user 글쓰기
-	// 
-	// http://localhost:8081/qnawriter
-	@GetMapping("/qnawriter")
-	public String qnaWriter() {
-		System.out.println("");
-		return "user_ComuQnAwrite";
-	}
-
+	/*
+	 * // 기능 : Qna_user 글쓰기(html연결함) // pram : QnaVO중 - ID(자동대입), nickname(로그인사용자
+	 * 닉네임 자동 대입), // no(자동대입), cate_Id, isPub(공개(Y)/비공개(N)), title, q //
+	 * http://localhost:8081/qnawriter
+	 * 
+	 * @GetMapping("/qnawriter") public String qnaWriter() { System.out.println("");
+	 * return "user_ComuQnAwrite"; }
+	 */
+	
+	
+	// 기능 : Qna_user 글쓰기 (위"/qnawriter 과 동일한 기능)
+	// param : QnaVO중 - ID(자동대입), nickname(로그인사용자 닉네임 자동 대입),
+	//        		   no(자동대입), cate_Id, isPub(공개(Y)/비공개(N)), title, q
 	// http://localhost:8081/qna/insert?nickname=yskim&no=1&cateId=aaaa&isPub=Y&title=title&q=question&cUser=yskim
 	@GetMapping("/insert")
 	public @ResponseBody String insert(QnaVO vo) {
 		System.out.println(className + ".insert() : " + vo.toString());
 		return service.insert(vo).toString();
 	}
-
+		
+	// 기능 : Qna_user 글 수정하기
+	// param : QnaVO 중 - ID(insert된 ID/자동대입), no(insert된 no/자동대입), cate_ID, isPub(공개(Y)/비공개(N)),
+	//	 	  			title, q
 	// http://localhost:8081/qna/update?title=cddcdd&q=aaaaa&cateId=bbbb&isPub=y&id=
 	@GetMapping("/qupdate")
 	public @ResponseBody String qupdate(QnaVO vo) {
@@ -56,12 +63,16 @@ public class QnaController {
 		return service.qupdate(vo).toString();
 	}
 
+	// 기능 : Qna_admin 답변쓰기
+	// param : QnaVO 중 - id(insert된 ID/자동대입), a
 	@GetMapping("/aupdate")
 	public @ResponseBody String aupdate(QnaVO vo) {
 		System.out.println(className + "Aupdate():" + vo.toString());
 		return service.aupdate(vo).toString();
 	}
-
+	
+	// 기능 : Qna_admin 게시글 삭제
+	// param : id(insert된 ID/자동대입)
 	// http://localhost:8081/qna/delete?id=
 	@GetMapping("/delete")
 	public @ResponseBody String delete(QnaVO vo) {
