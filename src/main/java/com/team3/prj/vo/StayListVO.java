1ec850ce4952363cbc7f81aba5069c291d53f1ed
xpfwm <xpfwm@192.168.0.102>
2021-03-27 20:35:06 +0900

    주석추가

--- src/main/java/com/team3/prj/vo/StayListVO.java
+++ src/main/java/com/team3/prj/vo/StayListVO.java
@@ -5,8 +5,8 @@ import lombok.Data;
 @Data
 public class StayListVO {
 
-	public String region;// 
-	public String title; //
+	public String region;// 지역
+	public String title; //숙소명
 	public String addr; //주소
 	public int weekday_Price; // 주중가격
 	public int weekend_Price; // 주말가격
