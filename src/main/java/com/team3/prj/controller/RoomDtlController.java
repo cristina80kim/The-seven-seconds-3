82c99d376d2fd137dfd62050cd7777ed8bb573df
maruhe <maruhe@DESKTOP-9HVMQ11>
2021-03-30 19:41:49 +0900

    // roomMst - 수정 // roomDtl - 등록(insert) 기능 추가

--- src/main/java/com/team3/prj/controller/RoomDtlController.java
+++ src/main/java/com/team3/prj/controller/RoomDtlController.java
@@ -1,13 +1,21 @@
 package com.team3.prj.controller;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
+import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 
+import com.team3.prj.etc.Libs;
 import com.team3.prj.service.RoomDtlService;
 import com.team3.prj.vo.RoomDtlVO;
 
@@ -27,11 +35,66 @@ public class RoomDtlController {
 		return service.selectAll();
 	}
 
-	// http://localhost:8081/qna/insert?nickname=yskim&no=1&cateId=aaaa&isPub=Y&title=title&q=question&cUser=yskim
-	/*
-	 * @GetMapping("/insert") public @ResponseBody String insert(RoomMstVO vo) {
-	 * System.out.println(className + ".insert(): " + vo.toString()); return
-	 * service.insert(vo).toString(); //@ }
-	 */
+	
+	@Value("${file.upload.directory}")
+	String uploadDir;
 
+	
+	
+//	기능 : 숙소룸 등록
+//	Param : roomDtlVO 중 - id(자동대입), mstId, roomName, typeOfRoom(공통코드),
+//						  numOfGuests, checkInTime, checkOutTime, weekdayPrice,
+//						  weekendPrice, pic1Org, pic1Uuid, pic2Org, pic2Uuid,
+//						  pic3Org, pic3Uuid, cUser
+	// http://localhost:8081/roomDtl/manager_RoomRegist
+	@GetMapping("/manager_RoomRegist")
+	public String manager_RoomRegist() {
+		return "manager_RoomRegist";
+	}
+	
+	@PostMapping(value = "/insert")
+	public @ResponseBody String insert(RoomDtlVO vo, @RequestParam("files") List<MultipartFile> files)
+			throws IllegalStateException, IOException {
+		String uploadPath = new File("").getAbsolutePath() + uploadDir;
+		String fileName = null;
+
+		if (files != null) {
+			int index = 0;
+			for (MultipartFile file : files) {
+				if (!file.isEmpty()) {
+					UUID uuid = UUID.randomUUID();
+					String destName = uuid.toString().replace("-", "");
+					String orgName = file.getOriginalFilename();
+					String ext = Libs.getFileExt(orgName);
+					String destPath = uploadPath + destName + ext;
+
+					System.out.println("원본파일명: " + orgName);
+					System.out.println("생성된 파일: " + destPath);
+
+					File dest = new File(destPath);
+					file.transferTo(dest);
+
+					if (index == 0) {
+						String uuidPic1 = destName + ext;
+						vo.setPic1Org(orgName);
+						vo.setPic1Uuid(uuidPic1);
+					} else if (index == 1) {
+						String uuidPic2 = destName + ext;
+						vo.setPic2Org(orgName);
+						vo.setPic2Uuid(uuidPic2);
+					} else {
+						String uuidPic3 = destName + ext;
+						vo.setPic3Org(orgName);
+						vo.setPic3Uuid(uuidPic3);
+					}
+					index++;
+				}
+			}
+		}
+		System.out.println(className + ".insert(): " + vo.toString());
+		return service.insert(vo).toString();
+	}
+
+	
+	
 }
--- src/main/java/com/team3/prj/vo/RoomDtlVO.java
+++ src/main/java/com/team3/prj/vo/RoomDtlVO.java
@@ -4,22 +4,23 @@ import lombok.Data;
 
 @Data
 public class RoomDtlVO {
-	private String id;
-	private String mst_id;
-	private String room_name;
-	private String num_of_guests;
-	private String check_in_time;
-	private String check_out_time;
-	private String weekday_price;
-	private String weekend_price;
-	private String pic1Org;
-	private String pic1Uuid;
-	private String pic2Org;
-	private String pic2Uuid;
-	private String pic3Org;
-	private String pic3Uuid;
-	private String cUser;
-	private String mUser;
-	private String cDate;
-	private String mDate;
+	private Long id;				// ID
+	private String mstId;			// 숙소ID
+	private String roomName;		// 룸명
+	private String typeOfRoom;		// 룸종류(공통코드)
+	private String numOfGuests;		// 인원수
+	private String checkInTime;		// 체크인 시간
+	private String checkOutTime;	// 체크아웃 시간
+	private String weekdayPrice;	// 주중 금액
+	private String weekendPrice;	// 주말(공휴일포함) 금액
+	private String pic1Org;			// 룸이미지 1
+	private String pic1Uuid;		// 룸이미지 UUID 1
+	private String pic2Org;			// 룸이미지 2
+	private String pic2Uuid;		// 룸이미지 UUID 2
+	private String pic3Org;			// 룸이미지 3
+	private String pic3Uuid;		// 룸이지미 UUID 3
+	private String cUser;			// 생성자 ID
+	private String mUser;			// 수정자 ID
+	private String cDate;			// 생성 날짜
+	private String mDate;			// 수정 날짜
 }
