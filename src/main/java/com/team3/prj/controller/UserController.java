3e6cea8f0ebd96304f5348ebe9242698297b8e04
Seong <sshyun89@naver.com>
2021-03-30 15:32:21 +0900

    user_join Controller 수정(post형식)

--- src/main/java/com/team3/prj/controller/UserController.java
+++ src/main/java/com/team3/prj/controller/UserController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -49,8 +51,8 @@ public class UserController {
 		return "user_Join";// user_Join.html
 	}
 	
-	@GetMapping("/join")
-	public @ResponseBody String join(UserVO vo) {
+	@PostMapping("/join")
+	public @ResponseBody String join(@RequestBody UserVO vo) {
 		System.out.println(className + ".join()");
 		System.out.println(vo);
 		return String.valueOf(userService.join(vo));
