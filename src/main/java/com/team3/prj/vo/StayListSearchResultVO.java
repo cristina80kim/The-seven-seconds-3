7b0848ef24bbbff41da0ee886111d36b91b6d97d
cristina80kim <cristina80kim@gmail.com>
2021-04-01 23:30:29 +0900

    staylist 검색 VO 추가

--- src/main/java/com/team3/prj/vo/StayListSearchResultVO.java
+++ src/main/java/com/team3/prj/vo/StayListSearchResultVO.java
@@ -12,7 +12,7 @@ public class StayListSearchResultVO {
     private String addr;            // 주소
     private String pic1Uuid;        // 수속 이미지 #1
     private String typeOfAcco;      // 숙박시설 유형
-    private String typeOfAccoName;  // 숙박시설 유형
+    private String typeOfAccoName;  // 숙박시설 유형(공통코드 class1='ROOM", class2="TYPE_OF_ACCO"인 name 값)
     private int weekdayPrice;       // 주중가격
     private int weekendPrice;       // 주말가격
     private int star;               // 별점
