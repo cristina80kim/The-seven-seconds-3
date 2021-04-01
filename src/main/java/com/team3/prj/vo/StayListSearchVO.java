8fc95d5caece4666fe15266fe2d996146bda02c3
cristina80kim <cristina80kim@gmail.com>
2021-04-02 01:00:30 +0900

    숙소/룸 검색 Query 작성중(호텔, 모텔,...클릭시 오류 있음)

--- src/main/java/com/team3/prj/vo/StayListSearchVO.java
+++ src/main/java/com/team3/prj/vo/StayListSearchVO.java
@@ -1,22 +1,24 @@
 package com.team3.prj.vo;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
 public class StayListSearchVO {
 //수정
-	private String mstId; //마스터아이
-	private String dtlid; //아이디	
-	private int weekdayPrice;
-	private int weekendPrice; 
-	private int numOfGuests;
-	private String isHotel; //호텔
-	private String isMotel; //모텔
-	private String isPension; //펜션/풀빌라
-	private String isGlamping;//글램핑/캠핑
-	private String isGuestHouse;//게하/한옥
-	private String isResort;//리조트/콘도
-	private String keyword; //검색창
-	private String checkin; //예약테이블 (체크인)
-	private String checkout; //예약테이블 (체크아웃)
+    private String mstId;       //마스터아이
+    private String dtlid;       //아이디   
+    private String keyword;     //검색어
+    private String checkIn;     //예약테이블(체크인)
+    private String checkOut;    //예약테이블(체크아웃)
+    private int numOfGuests;    //인원수
+    private int minPrice;       //가격(하한값)
+    private int maxPrice;       //가격(상한값)
+    private String isHotel;     //숙소유형(호텔)
+    private String isMotel;     //숙소유형(모텔)
+    private String isPension;   //숙소유형(펜션/풀빌라)
+    private String isGlamping;  //숙소유형(글램핑/캠핑)
+    private String isGuestHouse;//숙소유형(게하/한옥)
+    private String isResort;    //숙소유형(리조트/콘도)
 }
