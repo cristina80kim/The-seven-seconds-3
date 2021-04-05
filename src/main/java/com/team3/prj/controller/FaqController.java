d80c2b9ca3c475741e53a47aea485198fccbd604
maruhe <maruhe@DESKTOP-9HVMQ11>
2021-04-05 22:49:32 +0900

    faq commit

--- src/main/java/com/team3/prj/controller/FaqController.java
+++ src/main/java/com/team3/prj/controller/FaqController.java
@@ -29,7 +29,7 @@ public class FaqController {
 		System.out.println(className + "selectAll()");
 		return service.selectAll();
 	}
-	
+
 //	1개 출력
 //	http://localhost:8081/faq/selectOne?id=
 	@GetMapping("/selectOne")
@@ -37,7 +37,7 @@ public class FaqController {
 		System.out.println(className + "selectOne()");
 		return service.selectOne(vo);
 	}
-	
+
 //	검색
 //	http://localhost:8081/faq/search?cateId=&keyWord=
 	@GetMapping("/search")
@@ -45,23 +45,21 @@ public class FaqController {
 		System.out.println(className + "search()");
 		return service.search(svo);
 	}
-	
-	
-	
+
 //	Faq 등록
 //	http://localhost:8081/faq/FaqWriter
 	@GetMapping("/FaqWriter")
 	public String FaqWriter() {
 		return "admin_comu_FAQList_Write";
 	}
-	
+
 //	http://localhost:8081/faq/insert
 	@GetMapping("/insert")
 	public @ResponseBody String insert(FaqVO vo) {
 		System.out.println(className + "insert()" + vo.toString());
 		return service.insert(vo).toString();
 	}
-	
+
 //	Faq 수정
 //	http://localhost:8081/faq/update
 	@GetMapping("/update")
@@ -69,7 +67,7 @@ public class FaqController {
 		System.out.println(className + "update()" + vo.toString());
 		return service.update(vo).toString();
 	}
-	
+
 //	Faq 삭제
 //	http://localhost:8081/faq/delete?id=
 	@GetMapping("/delete")
@@ -78,29 +76,35 @@ public class FaqController {
 		return service.delete(vo).toString();
 	}
 
-	
 	// http://localhost:8081/faq/searchData
-    @GetMapping("/searchData")
-    public @ResponseBody Object getData4Toast3(FaqSearchVO svo) {
-        System.out.println(className + ".getData4Toast3()");
-        
-        List<FaqVO> lstQnaResult = service.search(svo);
-        return Libs.makeToastJsonResult(lstQnaResult);
-    }
-    
- // 기능 TOAST grid를 위한 검색 및 결과 작성	
-    // http://localhost:8081/faq/toastSearch?cateId=&keyWord=
-    @GetMapping("/toastSearch")
-    public @ResponseBody JSONObject ToastSearch(FaqSearchVO svo) {
-        System.out.println(className + ".faqToastSearch()");
-        System.out.println(svo);
-        return Libs.makeToastJsonResult(service.search(svo));  
-    }
-//    http://localhost:8081/faq/faqList
-    @GetMapping("/faqList")
-    public String faqList() {
-    	return "admin_comu_FAQList";
-    }
-    
-}
+	@GetMapping("/searchData")
+	public @ResponseBody Object getData4Toast3(FaqSearchVO svo) {
+		System.out.println(className + ".getData4Toast3()");
 
+		List<FaqVO> lstQnaResult = service.search(svo);
+		return Libs.makeToastJsonResult(lstQnaResult);
+	}
+
+	// 기능 TOAST grid를 위한 검색 및 결과 작성
+	// http://localhost:8081/faq/toastSearch?cateId=&keyWord=
+	@GetMapping("/toastSearch")
+	public @ResponseBody JSONObject ToastSearch(FaqSearchVO svo) {
+		System.out.println(className + ".faqToastSearch()");
+		System.out.println(svo);
+		return Libs.makeToastJsonResult(service.search(svo));
+	}
+
+//    http://localhost:8081/faq/adminList
+	@GetMapping("/adminList")
+	public String adminList() {
+		return "admin_comu_FAQList";
+	}
+
+//  http://localhost:8081/faq/userList
+	@GetMapping("/userList")
+	public String userList() {
+		return "user_ComuFAQ";
+
+	}
+
+}
