24254e324b8d3cd1b755c1b7635e5d08e8d64be2
maruhe <maruhe@DESKTOP-9HVMQ11>
2021-04-06 19:35:50 +0900

    공지사항 수정

--- src/main/java/com/team3/prj/controller/NoticeController.java
+++ src/main/java/com/team3/prj/controller/NoticeController.java
@@ -2,12 +2,14 @@ package com.team3.prj.controller;
 
 import java.util.List;
 
+import org.json.simple.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.team3.prj.etc.Libs;
 import com.team3.prj.service.NoticeService;
 import com.team3.prj.vo.NoticeSearchVO;
 import com.team3.prj.vo.NoticeVO;
@@ -51,7 +53,7 @@ public class NoticeController {
 		return "admin_comu_NotiList_Write";//.html
 	}
 	
-//	http://localhost:8081/noti/isert
+//	http://localhost:8081/noti/insert
 	@GetMapping("/insert")
 	public @ResponseBody String insert(NoticeVO vo) {
 		System.out.println(className + "insert()" + vo.toString());
@@ -76,5 +78,33 @@ public class NoticeController {
 		return service.delete(vo).toString();
 	}
 	
+	// http://localhost:8081/notice/searchData
+		@GetMapping("/searchData")
+		public @ResponseBody Object getDataToast(NoticeSearchVO svo) {
+			System.out.println(className + ".getDataToast()");
 
+			List<NoticeVO> lstQnaResult = service.search(svo);
+			return Libs.makeToastJsonResult(lstQnaResult);
+		}
+		
+		// http://localhost:8081/notice/toastSearch?cateId=&keyWord=
+		@GetMapping("/toastSearch")
+		public @ResponseBody JSONObject ToastSearch(NoticeSearchVO svo) {
+			System.out.println(className + ".NoticeToastSearch()");
+			System.out.println(svo);
+			return Libs.makeToastJsonResult(service.search(svo));
+		}
+		
+//	    http://localhost:8081/notice/adminList
+		@GetMapping("/adminList")
+		public String adminList() {
+			return "admin_comu_NotiList";
+		}
+		
+	//  http://localhost:8081/notice/searchList
+		   @GetMapping("/searchList")
+		   public String userList() {
+		      return "user_ComuNoti";
+
+		   }
 }
