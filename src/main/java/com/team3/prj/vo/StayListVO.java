240b2114d5877f1e03f67e3670294f467a1e2a9b
xpfwm <xpfwm@192.168.0.102>
2021-04-08 18:12:28 +0900

    room_dtl,mst 데이터 삽입

--- src/main/java/com/team3/prj/vo/StayListVO.java
+++ src/main/java/com/team3/prj/vo/StayListVO.java
@@ -1,14 +1,19 @@
 package com.team3.prj.vo;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
 public class StayListVO {
 
-	public String region;// 지역
-	public String title; //숙소명
-	public String addr; //주소
-	public int weekday_Price; // 주중가격
-	public int weekend_Price; // 주말가격
-	//public int star; // 별점
+    private String region;          // 지역구분
+    private String title;           // 숙소명
+    private String addr;            // 주소
+    private String pic1Uuid;        // 수속 이미지 #1
+    private String typeOfAcco;      // 숙박시설 유형
+    private String typeOfAccoName;  // 숙박시설 유형(공통코드 class1='ROOM", class2="TYPE_OF_ACCO"인 name 값)
+    private int weekdayPrice;       // 주중가격
+    private int weekendPrice;       // 주말가격
+    private int star;               // 별점
 }
