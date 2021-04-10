0c4bcee01149353adced4c3de18da8691d4e9b9b
maruhe <maruhe@DESKTOP-9HVMQ11>
2021-04-10 12:29:36 +0900

    qna 카테고리 출력 및 답변 유무 출력

--- src/main/java/com/team3/prj/vo/QnaVO.java
+++ src/main/java/com/team3/prj/vo/QnaVO.java
@@ -8,14 +8,17 @@ public class QnaVO {
 	private String nickname; //닉네임
 	private String no; //글번호
 	private String cate_id; //공통코드 faq_cate 참조
+	private String cate;	// 카테고리명
 	private String is_pub; //'Y' 공개글 'N' 비밀글
 	private String title; //제목
 	private String q; //질문 내용
 	private String a; //답변 내용
+	private String aox; //답변유무
 	private String c_user; // 생성자
 	private String c_date; // 생성날짜
 	private String m_user; // 수정자
 	private String m_date; // 수정날짜
+
 
 
 	 
