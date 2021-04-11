39e3eb3beeb4b89444a1141fee2d22f5fca27ef0
xpfwm <xpfwm@192.168.0.102>
2021-04-11 17:56:14 +0900

    roomlistopen 권한부여

--- src/main/java/com/team3/prj/controller/RoomListOpenController.java
+++ src/main/java/com/team3/prj/controller/RoomListOpenController.java
@@ -3,9 +3,19 @@ package com.team3.prj.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.team3.prj.etc.Common;
+
 @Controller
 public class RoomListOpenController {
-
+    private final String className =  "RoomListOpenController";
+    private final String className2 = className + ".";
+    
+    public String[][] getAuthorizations() {
+        return new String[][] {
+            {className2 + "managerRsrvOpen1", Common.strRoleAMUY},
+        };
+    }
+    
     //화면 231번
   //http://localhost:8081/managerRsrvOpen1 
     @GetMapping("/managerRsrvOpen1")
