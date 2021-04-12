6b4914697ed103d74af33f788304d45172715438
cristina80kim <cristina80kim@gmail.com>
2021-04-12 17:01:22 +0900

    role 추가

--- src/main/java/com/team3/prj/vo/UserInstanceVO.java
+++ src/main/java/com/team3/prj/vo/UserInstanceVO.java
@@ -11,14 +11,16 @@ public class UserInstanceVO {
     private String nickname;
     private String tel;
     private String email;
+    private String gen;
     private String role;
 
-    public UserInstanceVO(String id, String name, String nickname, String tel, String email, String role) {
+    public UserInstanceVO(String id, String name, String nickname, String tel, String email, String gen, String role) {
         this.id = id;
         this.name = name;
         this.nickname = nickname;
         this.tel = tel;
         this.email = email;
+        this.gen = gen;
         this.role = role;
     }
 
@@ -28,6 +30,7 @@ public class UserInstanceVO {
         this.nickname = vo.getNickname();
         this.tel = vo.getTel();
         this.email = vo.getEmail();
+        this.gen = vo.getGen();
         this.role = vo.getRole();
     }
 
